86c8c058c29463f5659e34116070f749f6ed51e6
phongpdt <phongpdt@datascience.com.vn>
2020-12-29 17:37:36 +0700

    read data from input

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from daos import Parameter
 from utils import Common
-from algorithm import Individual, Objectives, Population, NSGA
+from algorithm import Individual, Population, NSGA
 
 params = Parameter()
 
-params.process("data/data.txt")
+filename = input("Please input data file name (Ex: data.txt): \n")
 
-# print(params.t_duration)
-# print(Common.get_prev_tasks(params))
+params.process(f"data/{filename}")
+
 pop = Individual()
 pop.randomize(params=params)
 
 pop_init = Population().make_pop(params=params)
 best_allocate = NSGA().run(params=params, pop_init=pop_init)
 Common.printPop(best_allocate, params.humans, params.machines)
-# func = Objectives()
-# print(func.f_cost(individual=pop, params=params))
-# print(1 << param.humans)
-# print(pop.t_sched)
